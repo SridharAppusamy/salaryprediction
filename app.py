fa885d58b611f567e7d434612a5cf7e95b727ebe
SridharAppusamy <sridharappusamy@gmail.com>
2022-04-12 18:32:24 +0530

    example conflict

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def home():
 @app.route('/predict',methods=['POST'])
 def predict():
     '''
-    For rendering results on HTML GUI page
+    For rendering results on HTML GUI page test
     '''
     int_features = [int(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
